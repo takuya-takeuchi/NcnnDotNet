d4f5791722d16e58f2d93d67da68518f526b0e62
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2020-01-02 23:35:06 +0900

    feat: support ubuntu and centos (not tested)

---
+++ docker/build_runtime.ps1
@@ -0,0 +1,45 @@
+Param()
+
+$baseDockerfiles = Get-ChildItem  -Recurse base -include Dockerfile
+
+foreach($dockerfile in $baseDockerfiles)
+{
+   $relativePath = Resolve-Path $dockerfile -Relative
+   $dockerfileDirectory = Resolve-Path ((Get-ChildItem $relativePath).Directory.FullName) -Relative
+   $basetag = "ncnndotnet" + $dockerfileDirectory.Trim('.').Replace('\', '/')
+
+   Write-Host "Start 'docker build -t $basetag $dockerfileDirectory'" -ForegroundColor Green
+   docker build --force-rm=true -t $basetag $dockerfileDirectory
+
+   if ($lastexitcode -ne 0)
+   {
+      Write-Host "Failed 'docker build -t $basetag $dockerfileDirectory'" -ForegroundColor Red
+      exit -1
+   }
+
+   $dockerfileDirectory = $dockerfileDirectory.Replace('base', 'runtime')
+   if (Test-Path $dockerfileDirectory)
+   {
+      $runtimeDockerfileDirectory = $dockerfileDirectory
+      $runtimeNameBase = $dockerfileDirectory.Replace('\', '/').Trim('.')
+   }
+   else
+   {
+      $path = $dockerfileDirectory.Replace('\', '/').Split('/')
+      $os = $path[2]
+      $version = $path[3]
+      $runtimeDockerfileDirectory = Join-Path 'runtime' $os  | `
+                                    Join-Path -ChildPath $version -Resolve
+      $runtimeNameBase = $dockerfileDirectory.Replace('\', '/').Trim('.')
+   }
+
+   $runtimetag = "ncnndotnet" + $runtimeNameBase.Replace('base', 'runtime')
+   Write-Host "Start 'docker build -t $runtimetag $runtimeDockerfileDirectory --build-arg IMAGE_NAME=""$basetag""'" -ForegroundColor Green
+   docker build --force-rm=true -t $runtimetag $runtimeDockerfileDirectory --build-arg IMAGE_NAME="$basetag"
+
+   if ($lastexitcode -ne 0)
+   {
+      Write-Host "Failed 'docker build -t $runtimetag $runtimeDockerfileDirectory --build-arg IMAGE_NAME=""$basetag""'" -ForegroundColor Red
+      exit -1
+   }
+}
---
+++ nuget/BuildUbuntu16.ps1
@@ -0,0 +1,88 @@
+Param()
+
+# import class and function
+$ScriptPath = $PSScriptRoot
+$NcnnDotNetRoot = Split-Path $ScriptPath -Parent
+$NugetPath = Join-Path $NcnnDotNetRoot "nuget" | `
+             Join-Path -ChildPath "BuildUtils.ps1"
+import-module $NugetPath -function *
+
+$OperatingSystem="linux"
+$Distribution="ubuntu"
+$DistributionVersion="16"
+
+# Store current directory
+$Current = Get-Location
+$NcnnDotNetRoot = (Split-Path (Get-Location) -Parent)
+$NcnnDotNetSourceRoot = Join-Path $NcnnDotNetRoot src
+$DockerDir = Join-Path $Current docker
+
+Set-Location -Path $DockerDir
+
+$DockerFileDir = Join-Path $DockerDir build  | `
+                 Join-Path -ChildPath $Distribution | `
+                 Join-Path -ChildPath $DistributionVersion
+
+$BuildSourceHash = [Config]::GetBinaryLibraryLinuxHash()
+
+# https://github.com/dotnet/coreclr/issues/9265
+# linux-x86 does not support
+$BuildTargets = @()
+$BuildTargets += New-Object PSObject -Property @{ Platform = "desktop"; Target = "cpu";     Architecture = 64; Postfix = "/x64"; RID = "$OperatingSystem-x64"; }
+$BuildTargets += New-Object PSObject -Property @{ Platform = "desktop"; Target = "vulkan";  Architecture = 64; Postfix = "/x64"; RID = "$OperatingSystem-x64"; }
+
+foreach($BuildTarget in $BuildTargets)
+{
+   $platform = $BuildTarget.Platform
+   $target = $BuildTarget.Target
+   $architecture = $BuildTarget.Architecture
+   $rid = $BuildTarget.RID
+   $postfix = $BuildTarget.Postfix
+   $option = ""
+
+   $dockername = "ncnndotnet/build/$Distribution/$DistributionVersion/$Target" + $postfix
+   $imagename  = "ncnndotnet/devel/$Distribution/$DistributionVersion/$Target" + $postfix
+
+   $Config = [Config]::new($NcnnDotNetRoot, "Release", $target, $architecture, $platform, $option)
+   $libraryDir = Join-Path "artifacts" $Config.GetArtifactDirectoryName()
+   $build = $Config.GetBuildDirectoryName($OperatingSystem)
+
+   Write-Host "Start 'docker build -t $dockername $DockerFileDir --build-arg IMAGE_NAME=""$imagename""'" -ForegroundColor Green
+   docker build --force-rm=true -t $dockername $DockerFileDir --build-arg IMAGE_NAME="$imagename"
+
+   if ($lastexitcode -ne 0)
+   {
+      Set-Location -Path $Current
+      exit -1
+   }
+
+   # Build binary
+   foreach ($key in $BuildSourceHash.keys)
+   {
+      Write-Host "Start 'docker run --rm -v ""$($NcnnDotNetRoot):/opt/data/NcnnDotNet"" -e LOCAL_UID=$(id -u $env:USER) -e LOCAL_GID=$(id -g $env:USER) -t $dockername'" -ForegroundColor Green
+      docker run --rm `
+                  -v "$($NcnnDotNetRoot):/opt/data/NcnnDotNet" `
+                  -e "LOCAL_UID=$(id -u $env:USER)" `
+                  -e "LOCAL_GID=$(id -g $env:USER)" `
+                  -t "$dockername" $key $target $architecture $platform $option
+   
+      if ($lastexitcode -ne 0)
+      {
+         Set-Location -Path $Current
+         exit -1
+      }
+   }
+
+   # Copy output binary
+   foreach ($key in $BuildSourceHash.keys)
+   {
+      $srcDir = Join-Path $NcnnDotNetSourceRoot $key
+      $dll = $BuildSourceHash[$key]
+      $dstDir = Join-Path $Current $libraryDir
+
+      CopyToArtifact -srcDir $srcDir -build $build -libraryName $dll -dstDir $dstDir -rid $rid
+   }
+}
+
+# Move to Root directory 
+Set-Location -Path $Current
