0d055211aad061e91e590a05e5e63881580a7639
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2020-01-03 20:04:51 +0900

    fix: rename rid

--- nuget/BuildUbuntu16.ps1
+++ nuget/BuildUbuntu16.ps1
@@ -7,7 +7,7 @@ $NugetPath = Join-Path $NcnnDotNetRoot "nuget" | `
              Join-Path -ChildPath "BuildUtils.ps1"
 import-module $NugetPath -function *
 
-$OperatingSystem="linux"
+$OperatingSystem="ubuntu"
 $Distribution="ubuntu"
 $DistributionVersion="16"
 
