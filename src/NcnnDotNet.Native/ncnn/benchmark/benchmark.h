f956ed49bba3be94291ebb965f5e7e9339c300a4
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2020-01-12 18:44:19 +0900

    feat: add GetCurrentTime

---
+++ src/NcnnDotNet.Native/ncnn/benchmark/benchmark.h
@@ -0,0 +1,13 @@
+#ifndef _CPP_BENCHMARK_BENCHMARK_H_
+#define _CPP_BENCHMARK_BENCHMARK_H_
+
+#include "../export.h"
+#include <ncnn/benchmark.h>
+#include "../shared.h"
+
+DLLEXPORT double benchmark_get_current_time()
+{
+    return ncnn::get_current_time();
+}
+
+#endif
---
+++ src/NcnnDotNet/PInvoke/Benchmark/Ncnn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// ReSharper disable once CheckNamespace
+namespace NcnnDotNet
+{
+
+    internal sealed partial class NativeMethods
+    {
+
+        [DllImport(NativeLibrary, CallingConvention = CallingConvention)]
+        public static extern double benchmark_get_current_time();
+
+    }
+
+}
