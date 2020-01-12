416cc24cbf18ecc7ab5d2411dc614790c957a262
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2020-01-12 20:33:11 +0900

    feat: add DataReader

---
+++ src/NcnnDotNet/PInvoke/DataReader/DataReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// ReSharper disable once CheckNamespace
+namespace NcnnDotNet
+{
+
+    internal sealed partial class NativeMethods
+    {
+
+        [DllImport(NativeLibrary, CallingConvention = CallingConvention)]
+        public static extern IntPtr datareader_DataReader_new(IntPtr constructorFunction,
+                                                              IntPtr destructorFunction,
+                                                              IntPtr scanFunction,
+                                                              IntPtr readFunction);
+
+        [DllImport(NativeLibrary, CallingConvention = CallingConvention)]
+        public static extern void datareader_DataReader_delete(IntPtr reader);
+
+    }
+
+}
