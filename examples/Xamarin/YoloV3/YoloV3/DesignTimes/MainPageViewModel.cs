0fe36f50e3370b7631131a84660f4436a8d4dc8f
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-05-15 22:05:40 +0900

    fix

---
+++ examples/Xamarin/YoloV3/YoloV3/DesignTimes/MainPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Commands;
+using Xamarin.Forms;
+using YoloV3.ViewModels.Interfaces;
+
+namespace YoloV3.DesignTimes
+{
+
+    public class MainPageViewModel : IMainPageViewModel
+    {
+
+        #region Constructors
+
+        public MainPageViewModel()
+        {
+            this.Title = "Yolo V3";
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public DelegateCommand FilePickCommand
+        {
+            get;
+        }
+
+        public ImageSource SelectedImage
+        {
+            get;
+        }
+
+        public string Title
+        {
+            get;
+        }
+
+        #endregion
+
+    }
+
+}
--- examples/Xamarin/YoloV3/YoloV3/ViewModels/MainPageViewModel.cs
+++ examples/Xamarin/YoloV3/YoloV3/ViewModels/MainPageViewModel.cs
@@ -1,19 +1,65 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+using Prism.Commands;
 using Prism.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using YoloV3.ViewModels.Interfaces;
 
 namespace YoloV3.ViewModels
 {
-    public class MainPageViewModel : ViewModelBase
+
+    public class MainPageViewModel : ViewModelBase, IMainPageViewModel
     {
+
+        #region Constructors
+
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
-            Title = "Main Page";
+            Title = "Yolo V3";
+
+            this._FilePickCommand = new Lazy<DelegateCommand>(this.FilePickCommandFactory);
         }
+
+        #endregion
+
+        #region Constructors
+
+        private readonly Lazy<DelegateCommand> _FilePickCommand;
+
+        private DelegateCommand FilePickCommandFactory()
+        {
+            return new DelegateCommand(async () =>
+            {
+                var result = await FilePicker.PickAsync(new PickOptions
+                {
+                    PickerTitle = "Please select a image file to detect object",
+                    FileTypes = FilePickerFileType.Images
+                });
+
+                //Text = $"File Name: {result.FileName}";
+                var stream = await result.OpenReadAsync();
+                this.SelectedImage = ImageSource.FromStream(() => stream);
+            });
+        }
+
+        public DelegateCommand FilePickCommand => this._FilePickCommand.Value;
+
+        private ImageSource _SelectedImage;
+
+        public ImageSource SelectedImage
+        {
+            get => this._SelectedImage;
+            private set
+            {
+                this._SelectedImage = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        #endregion
+
     }
+
 }
