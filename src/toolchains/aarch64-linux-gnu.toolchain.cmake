75d350b7e29e9d1d3eae5af3c18de672b7be76f7
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-07-19 01:53:58 +0900

    fix

--- src/toolchains/aarch64-linux-gnu.toolchain.cmake
+++ src/toolchains/aarch64-linux-gnu.toolchain.cmake
@@ -11,6 +11,9 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_C_FLAGS "-march=armv8-a")
 set(CMAKE_CXX_FLAGS "-march=armv8-a")
 
+# Binary is not be able to be executed in host, set 0 (success) forcibly
+set(THREADS_PTHREAD_ARG "0" CACHE STRING "Forcibly set by CMakeLists.txt." FORCE)
+
 # cache flags
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "c++ flags")
