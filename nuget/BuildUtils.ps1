c0f09a60ecf16ee107f22307c9ec5dab813585c9
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-12-25 14:20:21 +0900

    fix

--- nuget/BuildUtils.ps1
+++ nuget/BuildUtils.ps1
@@ -1702,13 +1702,17 @@ class ThirdPartyBuilder
                                  Join-Path -Childpath libMoltenVK.a
 
                # use libc++ rather than libstdc++
-               # * Add CMAKE_THREAD_LIBS_INIT="-lpthread" because CMake failed to find_package(Threads)
+               # * Fix for PThread library not in path
+               #     -D CMAKE_THREAD_LIBS_INIT="-lpthread" `
+               #     -D CMAKE_HAVE_THREADS_LIBRARY=1 `
+               #     -D CMAKE_USE_WIN32_THREADS_INIT=0 `
+               #     -D CMAKE_USE_PTHREADS_INIT=1 `
                # * omp.h is missing so remove the following arguments
-               # -D OpenMP_C_FLAGS=`"-Xclang -fopenmp`" `
-               # -D OpenMP_CXX_FLAGS=`"-Xclang -fopenmp`" `
-               # -D OpenMP_C_LIB_NAMES=`"libomp`" `
-               # -D OpenMP_CXX_LIB_NAMES=`"libomp`" `
-               # -D OpenMP_libomp_LIBRARY=`"${developerDir}/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/libomp.a`" `
+               #     -D OpenMP_C_FLAGS=`"-Xclang -fopenmp`" `
+               #     -D OpenMP_CXX_FLAGS=`"-Xclang -fopenmp`" `
+               #     -D OpenMP_C_LIB_NAMES=`"libomp`" `
+               #     -D OpenMP_CXX_LIB_NAMES=`"libomp`" `
+               #     -D OpenMP_libomp_LIBRARY=`"${developerDir}/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/libomp.a`" `
                Write-Host "   cmake -D CMAKE_BUILD_TYPE=$Configuration `
          -D CMAKE_CXX_FLAGS=`"-std=c++11 -stdlib=libc++ -static`" `
          -D CMAKE_EXE_LINKER_FLAGS=`"-std=c++11 -stdlib=libc++ -static`" `
@@ -1726,6 +1730,9 @@ class ThirdPartyBuilder
          -D WITH_LAYER_spp:BOOL=${WITH_LAYER_spp} `
          -D WITH_LAYER_tile:BOOL=${WITH_LAYER_tile} `
          -D CMAKE_THREAD_LIBS_INIT=`"-lpthread`" `
+         -D CMAKE_HAVE_THREADS_LIBRARY=1 `
+         -D CMAKE_USE_WIN32_THREADS_INIT=0 `
+         -D CMAKE_USE_PTHREADS_INIT=1 `
          -D OpenCV_DIR=`"${installOpenCVDir}`" `
          $ncnnDir" -ForegroundColor Yellow
                cmake -D CMAKE_BUILD_TYPE=$Configuration `
@@ -1745,6 +1752,9 @@ class ThirdPartyBuilder
                      -D WITH_LAYER_spp:BOOL=${WITH_LAYER_spp} `
                      -D WITH_LAYER_tile:BOOL=${WITH_LAYER_tile} `
                      -D CMAKE_THREAD_LIBS_INIT="-lpthread" `
+                     -D CMAKE_HAVE_THREADS_LIBRARY=1 `
+                     -D CMAKE_USE_WIN32_THREADS_INIT=0 `
+                     -D CMAKE_USE_PTHREADS_INIT=1 `
                      -D OpenCV_DIR="${installOpenCVDir}" `
                      $ncnnDir
 
